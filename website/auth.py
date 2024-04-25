68ff07abe09041ebed97c49a76eed2daa0473788
wasif <mughalwasif04@gmail.com>
2024-04-25 22:00:56 +0500

    New files and code added

--- website/auth.py
+++ website/auth.py
@@ -1,19 +1,25 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET','POST'])
 def login():
-    return "<p> Login </p>"
+    return render_template('login.html')
 
 @auth.route('/logout')
 def logout():
     return "<p> Logout </p>"
 
-@auth.route('/sign-in')
-def sign_in():
-    return "<p> Sign in </p>"
-
-@auth.route('/sign-up')
-def sign_up():
-    return "<p> Sign Up </p>"
+@auth.route('/register', methods=['GET','POST'])
+def register():
+    if request.method == 'POST':
+        first_name = request.form.get('Firstname')
+        last_name = request.form.get('lastname')
+        username = request.form.get('username')
+        email = request.form.get('email')
+        password = request.form.get('password')
+        confirm_password = request.form.get('confirm_password')
+        
+        if len(email) < 4:
+            pass
+    return render_template('register.html')
