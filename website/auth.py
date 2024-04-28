1080114001ca892f2964dd9b31c4f06298274b79
wasif <mughalwasif04@gmail.com>
2024-04-28 14:31:14 +0500

    Edit the file

--- website/auth.py
+++ website/auth.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for
-from models import User
+from .models import User
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
 
@@ -16,30 +16,30 @@ def logout():
 @auth.route('/register', methods=['GET','POST'])
 def register():
     if request.method == 'POST':
-        first_name = request.form.get('Firstname')
-        last_name = request.form.get('lastname')
+        first_name = request.form.get('first_name')
+        last_name = request.form.get('last_name')
         username = request.form.get('username')
         email = request.form.get('email')
         password = request.form.get('password')
         confirm_password = request.form.get('confirm_password')
-        
-        if len(email) < 4:
-            flash("Email must be greater than 3 characters.", category='error')
-        elif len(first_name) < 10:
+
+        if len(first_name) < 2:
             flash("first name must be greater than 10 characters.", category='error')
-        elif len(last_name) < 10:
+        elif len(last_name) < 2:
             flash("last name must be greater than 10 characters.", category='error')
-        elif len(username) < 9:
+        elif len(username) < 2:
             flash("user name must not have spaces between them.", category='error')
+        elif len(email) < 5:
+            flash("Email must be greater than 10 characters.", category='error')
         elif password != confirm_password:
             flash("Password don\'t match.", category='error')
-        elif password < 7:
+        elif len(password) < 7:
             flash('Password contain special characters')
         else:
             new_user = User(email=email, first_name=first_name, last_name=last_name,
-                            username=username, password=generate_password_hash(password, method='sha256'))
+                            username=username, password=generate_password_hash(password))
             db.session.add(new_user)
             db.session.commit()
             flash('Account created!', category='success')
-            return redirect(url_for('view.home'))
+            return redirect(url_for('views.home'))
     return render_template('register.html')
--- website/models.py
+++ website/models.py
@@ -12,7 +12,9 @@ class Note(db.Model):
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(150))
+    last_name = db.Column(db.String(150))
+    username = db.Column(db.String(150))
     email = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
-    first_name = db.Column(db.String(150))
     notes = db.relationship("Note")
